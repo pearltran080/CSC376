7325384aa6954b72ee552aaa2e21991d9279aa5e
pearltran080 <76570762+pearltran080@users.noreply.github.com>
2022-01-21 12:36:14 -0600

    refactored select.c

--- assign1/select.c
+++ assign1/select.c
@@ -41,25 +41,11 @@ int		main		(int	argc,
 		exit(EXIT_FAILURE);
 	}
 
-	// fgets(line, LINE_LEN, stdin);
-
-	int i = 0;
-	int start = 0;
-	int columns = 0;
-	int iColumn;
-
-	while (scanf("%c", &line[i]) != EOF) {
-		if (line[i] == '\n')  {
-			columns = i-start;
-			if (columns == passThruLen) {
-				for (iColumn = start; iColumn < i; iColumn++) {
-					printf("%c", line[iColumn]);
-				}
-				printf("\n");
-			}
-			start = i+1;	// set start to index after discovered "new line"
+	while(fgets(line, LINE_LEN, stdin) != NULL) {
+		if (line[passThruLen] == '\n') {
+			printf("%s", line);
+			line[passThruLen] = '\0';
 		}
-		i++;
 	}
 
   return(EXIT_SUCCESS);
