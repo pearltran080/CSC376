435965eb3df3efd4037cdcdddc7a5fef1fd46d30
pearltran080 <76570762+pearltran080@users.noreply.github.com>
2022-01-21 23:45:49 -0600

    revsort update

--- assign1/lsRevsortSelect.c
+++ assign1/lsRevsortSelect.c
@@ -35,41 +35,106 @@ int		main		(int		argc,
 
   //  YOUR CODE HERE
 
+	if (argc > 1) {
+		passThruLen = strtol(argv[1], &cPtr, 10);
+
+		if (passThruLen < 0) {
+			fprintf(stderr,"Number format error\n");
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		fprintf(stderr,
+        "Usage:\tlsRevsortSelect num\n"
+        "Where 'num' is a non-negative int telling"
+        " the column widths to allow through.\n"
+       );
+		exit(EXIT_FAILURE);
+	}
+
   //  ls
   //
   int	lsToSort[2];
   pid_t	lsChild;
 
   //  YOUR CODE HERE
+	pipe(lsToSort);
+	lsChild = fork();
 
+	if (lsChild == 0) {
+		dup2(lsToSort[1], STDOUT_FILENO);
+		close(lsToSort[0]);
+		close(lsToSort[1]);
+		printf("lschild");
+		execl("ls", "ls", NULL);
 
+	} else if (lsChild > 0) {
+		close(lsToSort[1]);
+		printf("lsparent");
+	} else {
+		exit(EXIT_FAILURE);
+
+	}
   //  sort
   //
   int	sortToSelect[2];
   pid_t	sortChild;
 
   //  YOUR CODE HERE
+	pipe(sortToSelect);
+	sortChild = fork();
 
+	if (sortChild == 0) {
+		dup2(sortToSelect[1], STDOUT_FILENO);
+		close(sortToSelect[0]);
+		close(sortToSelect[1]);
+
+		dup2(lsToSort[0], STDIN_FILENO);
+		close(lsToSort[0]);
+		printf("sortchild");
+		execl("sort", "sort", "-r", NULL);
+
+	} else if (sortChild > 0){
+		close(lsToSort[0]);
+		close(sortToSelect[1]);
+		printf("sortparent");
+	} else {
+		exit(EXIT_FAILURE);
+
+	}
 
   //  select
   int	selectToParent[2];
   pid_t	selectChild;
 
   //  YOUR CODE HERE
+	pipe(selectToParent);
+	selectChild = fork();
 
+	if (selectChild == 0) {
+		close(selectToParent[0]);
+		close(selectToParent[1]);
+		printf("selectchild");
+		execl("select", "select", argv[1], NULL);
 
-  //  Receive and print output:
-  char		line[LINE_LEN];
-  int		numBytes;
+	} else if (selectChild > 0) {
+	  //  Receive and print output:
+	  char		line[LINE_LEN];
+	  int		numBytes;
+		printf("selectparent");
+	  while  ( (numBytes=read(selectToParent[STDIN_FILENO],line,LINE_LEN-1)) > 0 )
+	  {
+	    line[numBytes]	= '\0';
+	    printf("%s",line);
+	  }
 
-  while  ( (numBytes=read(selectToParent[STDIN_FILENO],line,LINE_LEN-1)) > 0 )
-  {
-    line[numBytes]	= '\0';
-    printf("%s",line);
-  }
+	  close(selectToParent[STDIN_FILENO]);
 
-  close(selectToParent[STDIN_FILENO]);
+	  //  YOUR CODE HERE
+		wait(NULL);
 
-  //  YOUR CODE HERE
+	} else {
+		exit(EXIT_FAILURE);
+
+	}
   return(EXIT_SUCCESS);
 }
